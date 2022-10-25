aedf83886e7c0dd36ab13ebb9a1caf6d81fe9444
elena <elena@10.3.61.187>
2022-10-25 11:33:50 +0200

    TP2 manip moussaka

---
+++ TP2_manip_ELZIERE/src/tp2_manip_elziere/Moussaka.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_elziere;
+
+/**
+ *
+ * @author elena
+ */
+public class Moussaka {
+    int nbCalories;
+    
+    public Moussaka ( int donnee){
+        nbCalories = donnee;
+    }
+    
+public String toString (){
+    return String.valueOf( nbCalories);
+} 
+}
