32dcb144bad5e6fd7ef4efb396fcffa01d6233c5
elena <elena@10.3.61.187>
2022-10-25 11:33:38 +0200

    TP2 manip tartiflette

---
+++ TP2_manip_ELZIERE/src/tp2_manip_elziere/Tartiflette.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_elziere;
+
+/**
+ *
+ * @author elena
+ */
+public class Tartiflette {
+    int nbCalories;
+    
+    public Tartiflette ( int donnee){
+        nbCalories = donnee; 
+    }
+}
