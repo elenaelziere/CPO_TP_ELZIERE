5ac2b597c1b9361731da513f9f3d192158272c6c
elena <elena@10.3.61.187>
2022-10-25 11:45:09 +0200

    TP2 manip  version 2

--- TP2_manip_ELZIERE/src/tp2_manip_elziere/TP2_manip_ELZIERE.java
+++ TP2_manip_ELZIERE/src/tp2_manip_elziere/TP2_manip_ELZIERE.java
@@ -16,15 +16,15 @@ public class TP2_manip_ELZIERE {
     public static void main(String[] args) {
         // TODO code application logic here
         
-    Tartiflette assiette1 = new Tartiflette(500) ;
-    Tartiflette assiette2 = new Tartiflette(600) ; 
+    Tartiflette assiette1 = new Tartiflette(500) ;//nombre de calories de l'assiette 1
+    Tartiflette assiette2 = new Tartiflette(600) ; //nombre de calories de l'assiette 2
     assiette2.nbCalories = 400;
-    Tartiflette assiette3 = assiette2 ;
+    Tartiflette assiette3 = assiette2 ;//assignation de la valeur de l'assiette 2 à 3
     
     System.out.println("nb de calories de Assiette 2 : " + assiette2.nbCalories) ;
     System.out.println("nb de calories de Assiette 3 : " + assiette3.nbCalories) ;
     
-    assiette2 = assiette1;
+    assiette2 = assiette1;//échange de la valeur de lassiette 2 et 1
     assiette1 = assiette3;
     
     Moussaka [] tableau = new Moussaka[10];
